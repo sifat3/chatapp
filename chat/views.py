25e7c28fbf3d2c4f92a066fd77bcc8467b667bd4
Ashraful Haque Sifat <ahsifat321@gmail.com>
2023-10-22 20:59:32 +0600

    22/10/23

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def home(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username, password)
+        if user is not None:
+            login(request, user)
+            messages.success("Login successful!")
+            return redirect('home')
+        else:
+            messages.success('Invalid Username or Password, Please Try Again.')
+            return redirect('home')
+
+
     return render(request, 'chat/home.html')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    return render(request, 'chat/signup.html')
+
+
